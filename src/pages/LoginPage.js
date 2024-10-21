7a48325d4d0ed2b6b67e11d39e48bd942a153496
gganj123 <zwol10@naver.com>
2024-10-21 22:55:45 +0900

    fix : 에러 메세지 관리

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -18,21 +18,23 @@ const LoginPage = () => {
     try {
       const response = await api.post("/user/login", { email, password });
       console.log("ress", response);
+
       if (response.status === 200) {
         setUser(response.data.user);
         sessionStorage.setItem("token", response.data.token);
-        api.defaults.headers["aithorization"] = "Bearer " + response.data.token;
+        api.defaults.headers["authorization"] = "Bearer " + response.data.token;
         navigate("/");
+      } else {
+        throw new Error(response.data.error || "로그인에 실패했습니다.");
       }
-      throw new Error(response.message);
     } catch (error) {
-      setError(error.message);
+      console.log(error);
+      setError(error.error || "서버 오류가 발생했습니다.");
     }
   };
 
   return (
     <div className="display-center">
-      {error && <div className="red-error">{error}</div>}
       <Form className="login-box" onSubmit={handleLogin}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -52,6 +54,8 @@ const LoginPage = () => {
             onChange={(event) => setPassword(event.target.value)}
           />
         </Form.Group>
+        {error && <div className="red-error">{error}</div>}
+
         <div className="button-box">
           <Button type="submit" className="button-primary">
             Login
