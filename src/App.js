cdd69f9230035f17f21e83b0b4bc91d2a9c4d7bc
gganj123 <zwol10@naver.com>
2024-10-22 19:43:06 +0900

    study : 토큰인증받아서 받은 유저정보 저장하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Routes, Route, Router } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
-import { ToastContainer } from "react-toastify";
+import PrivateRoute from "./route/PrivateRoute";
+import api from "./utils/api";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const getUSer = async () => {
+    try {
+      const storedToken = sessionStorage.getItem("token");
+      if (storedToken) {
+        const response = await api.get("/user/me");
+        setUser(response.data.user);
+      }
+    } catch (error) {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    getUSer();
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute user={user}>
+            <TodoPage />
+          </PrivateRoute>
+        }
+      />
       <Route path="/register" element={<RegisterPage />} />
 
       <Route path="/login" element={<LoginPage />} />
